c82c0b73e172b202a6accffc28c6c13e9f0e821b
fanwenjie <fanwj@mail.ustc.edu.cn>
2016-12-31 11:49:46 +0800

    update

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 @echo off
-clang -Wno-deprecated-declarations -O3 -mavx -fopenmp  batch.c predict.c main.c -o LeNet_5.exe
+clang -Wno-deprecated-declarations -O3 -mavx -fopenmp  batch.c init.c main.c -o LeNet_5.exe
 if exist LeNet_5.exe editbin /Stack:8000000 LeNet_5.exe
--- predict.c
+++ predict.c
@@ -1,7 +1,5 @@
 #include "lenet.h"
 #include <math.h>
-#include <time.h>
-#include <stdlib.h>
 
 #ifndef __AVX__
 #error Please Add Build Variant -mavx
@@ -127,17 +125,6 @@ uint8_t predict(LeNet5 *lenet, image_t input,uint8_t labelCount)
 	load_input(&features, input);
 	forward(lenet, &features, relu);
 	return get_result(&features, labelCount);
-}
-
-void initial(LeNet5 *lenet)
-{
-    srand((unsigned int)time(0));
-	for (double *pos = (double *)lenet->weight0_1; pos < (double *)lenet->bias0_1; *pos++ = (2.0 / RAND_MAX) * rand() - 1);
-	for (double *pos = (double *)lenet->weight0_1; pos < (double *)lenet->weight2_3; *pos++ *= sqrt(6.0 / (LENGTH_KERNEL0 * LENGTH_KERNEL0 * (LAYER0 + LAYER1))));
-	for (double *pos = (double *)lenet->weight2_3; pos < (double *)lenet->weight4_5; *pos++ *= sqrt(6.0 / (LENGTH_KERNEL0 * LENGTH_KERNEL0 * (LAYER2 + LAYER3))));
-	for (double *pos = (double *)lenet->weight4_5; pos < (double *)lenet->weight5_6; *pos++ *= sqrt(6.0 / (LENGTH_KERNEL1 * LENGTH_KERNEL1 * (LAYER4 + LAYER5))));
-	for (double *pos = (double *)lenet->weight5_6; pos < (double *)lenet->bias0_1; *pos++ *= sqrt(6.0 / (LAYER5 + OUTPUT)));
-	for (int *pos = (int *)lenet->bias0_1; pos < (int *)(lenet + 1); *pos++ = 0);
 }
 
 const static unsigned long long mask[] ={0x8000000000000000L,0x8000000000000000L,0x8000000000000000L,0x8000000000000000L,0,0,0,0};
